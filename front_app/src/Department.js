a768f2d0eba67bd58d85fc41985048f73d72d030
FabTrindade <fabrtrindade@gmail.com>
2022-04-20 19:26:25 -0300

    Fishing a table to show Department info.

--- front_app/src/Department.js
+++ front_app/src/Department.js
@@ -11,7 +11,7 @@ export class Department extends Component{
         fetch(process.env.REACT_APP_API + 'department')
         .then(response => response.json())
         .then(data => {
-            this.setSate({deps:data});
+            this.setState({deps:data});
         });
     }
 
@@ -29,15 +29,17 @@ export class Department extends Component{
             <div>
                 <Table className="mt-4" striped bordered hover size="sm">
                     <thead>
-                        <tr>DepartmentId</tr>
-                        <tr>DepartmentName</tr>
-                        <tr>Options</tr>
+                        <tr>
+                            <th>DepartmentId</th>
+                            <th>DepartmentName</th>
+                            <th>Options</th>
+                        </tr>
                     </thead>
                     <tbody>
                         {deps.map(dep=>
-                            <tr key={dep.DepartmentId }>
-                                <td>DepartmentId</td>
-                                <td>DepartmentName</td>
+                            <tr key={dep.DepartmentId}>
+                                <td>{dep.DepartmentId}</td>
+                                <td>{dep.DepartmentName}</td>
                                 <td>Edit / Delete</td>
                             </tr>)}
                     </tbody>
